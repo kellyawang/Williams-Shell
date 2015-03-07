526efee650dca12b48fe6318e5f28de4ecda7b39
Kelly Wang <16kw6@panic14.(none)>
2015-03-06 23:21:50 -0500

    First commit to git.

---
+++ wsh.c
@@ -0,0 +1,303 @@
+//(c)2015 Kelly Wang and Juan Mena
+
+//This is a program that implements a Bourne-like shell from scratch called the Williams Shell. It has a few built in commands and job control similar to bash.  
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "hash.h"
+
+#define PTR_ADD(p,i) (((char*)p)+(i))
+
+//A helpful definition to have whenever we're making a system call
+//the 2 ()'s around call and docstring ensure that call makes an assignment if it is an operator
+// Sample usage:
+//SYSCALL(f = open(...), "opening input"); 
+#define SYSCALL(call, docstring) if ((call) == -1) { perror((docstring)); exit(errno) }
+
+//A helpful definition for debugging
+#define debugPrint(args) if (debug) { fprintf(stderr, args) }
+
+/*
+ *Parse function breaks the input into tokens
+ * size refers to the size of the bufffer
+ */
+char *parse(char *buffer, int *counter, int size, int *flag){
+
+  int NUMBER_OF_TESTS = 8;
+  char *test[] = {" ","\n",";",":","#","|", ">","<"};
+
+  int i;
+  int tokenSize = 512;
+  int temp = 512;
+
+  /*
+   *This is where we process the front part of the string. 
+   * "next" is buffer minus the first "counter" amount of characters
+   */
+  char *next = PTR_ADD(buffer,*counter);
+  
+  /* 
+   * This while loop is for ignoring all of the white space 
+   * in the buffer
+   * strcspn() calculates the length of the initial segment of s up to the reject character
+*/
+  while(*counter < size && strcspn(next, " ") == 0 ){
+    //printf("In loop \n");
+    next = PTR_ADD(next,1);
+    *counter = *counter + 1;
+    
+  }
+  //printf("after loop: %i \n", *counter);
+  
+  /*
+   *Here we check for tokens, the smallest sized token is returned
+   *Because all token sizes start counting from the beginning
+   *So the smallest token is the first token in the string
+   */
+  char *first = strndup(next, 1); 
+  for(i = 0; i < NUMBER_OF_TESTS; i++) {
+      if(i > 1 && strspn(first,test[i]) == 1 ){ //in case there's multiple ;'s or #'s 
+	temp = 1;//strspn(next,test[i]);
+      } else{
+	temp = strcspn(next,test[i]);
+      }
+      
+      //temp is size of current token being processed
+      if(temp < tokenSize){
+	tokenSize = temp;
+	
+      }
+    }
+  
+  //printf("Size of token: %i\n", tokenSize);
+
+  if(tokenSize == 0){
+    *flag = 1;
+    return ";";
+  }  
+  
+  *counter = tokenSize + *counter; //done processing first token, next iteration starts where we left off
+  return strndup(next, tokenSize); 
+  
+}
+
+
+/*
+ * Print documentation for all built in commands
+ */
+void help() {
+  printf("Type 'help' for documentation for all built in commands supported by the Williams shell.\n\n");
+
+  printf("exit - halts the shell interpretation.\n");
+  printf("kill - terminates jobs given an identifier of the targeted job.\n");
+  printf("jobs - displays a list of all outstanding jobs, with an identifier.\n");
+  printf("cd - changes the working directory of the shell.\n");  
+
+}
+
+
+/*
+ * Identify and execute built in commands. If not built-in, its an executable
+ */
+void builtin(char *command) {
+  //if "exit" "help" "jobs" or "kill" built in function, executes the command, return 1 for now
+  //else command is an executable, return 0
+  if (strcmp(command, "help") == 0) {
+    help();
+
+  } else if (strcmp(command, "jobs") == 0) {
+    //return 0;
+
+  } else if (strcmp(command, "kill") == 0) {
+    //return 0;
+
+  } else if (strcmp(command, "cd") == 0) {
+    //return 0;
+
+  } else if(strcmp(command, "exit") == 0) {
+    exit(0);
+  }
+  
+  //return 0;
+  
+}
+
+
+/*
+ * Return a hash table containing executable names and paths of all executables 
+ */
+hash getPath() {
+
+  hash h = ht_alloc(997);
+
+  // get path:
+  char *path = strdup(getenv("PATH"));	/* getenv(3) */
+  char name[1024], basename[256];
+  char *dirname;
+  DIR *dir;
+  // split path members
+  while ((dirname = strsep(&path,":"))) { /* strsep(3) */
+    // open directory file
+    dir = opendir(dirname);		  /* opendir(3) */
+    if (dir) {
+      struct dirent *de;	/* dirent(5) */
+      while ((de = readdir(dir))) { /* readdir(3) */
+	int type = de->d_type;
+	// check regular files....
+	if (type & DT_REG) {
+	  strcpy(name,dirname);	/* strcpy(3) */
+	  strcat(name,"/");	/* strcat(3) */
+	  strcpy(basename,de->d_name);
+	  strcat(name,basename);
+	  // ...that are executable
+	  if (0 == access(name,X_OK)) { /* access(2) */
+	    // add to database if they've not been encountered before
+	    if (!ht_get(h,basename)) {
+	      // enter into table, but:
+	      // make copies of key and value to void poisoning
+	      ht_put(h,strdup(basename),strdup(name)); /* strdup(3) */
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return h;
+}
+
+
+
+/*
+ * Main method to run the shell, parse each command from the user
+ * We read in and execute commands one by one as we get to them because they may modify 
+ * later commands
+*/
+int main (int argc, char **argv) {
+  
+  int bSize = 512;    //default buffer size
+  char buffer[bSize]; //create a buffer to store strings
+  char *token;  
+  int counter;
+  int flag = 0;
+  int tokenIndex;
+  char *tokenArray[bSize];
+  pid_t *parentIds = (pid_t*)malloc(sizeof(pid_t));
+  int pidIndex = 0;
+  int pidSize = 1;
+  
+  // Populate the hash table to store executables and their full path specifications.
+  hash h = getPath();
+  
+  printf("*******Welcome! You are now running the Williams Shell*******\n(c) 2015 Juan Mena and Kelly Wang.\n->");
+
+  //while there is something in the buffer continue prompting the user, read in bSize characters or up to \n or EOF
+  while(buffer == fgets(buffer, bSize, stdin)){
+    //printf("%s \n", buffer);
+    flag = 0; //set "flag" = 0 each time you start reading the buffer 
+    counter = 0; //index along the buffer we're looking at
+    
+    tokenIndex = 0;
+    
+    while(counter < bSize) {    
+      token = parse(buffer, &counter, bSize, &flag);
+      //      printf("counter: %i\n", counter);
+
+      //add each token to array
+      
+      tokenArray[tokenIndex] = token;
+      /*
+      if(token != 0){
+	printf("token array at index %d: %s\n", tokenIndex, tokenArray[tokenIndex]);
+      }
+      printf("Past token 0 \n");  
+      */    
+      //if the current token is an ;
+      //Then this is the end of the command
+      //Execute the command, and set tokenIndex to zero
+      //Or else just increment tokenIndex
+      if(strcmp(token,";") == 0){
+	//First try and see if the first token is a built in
+	if(tokenArray[0] != 0){
+	  builtin(tokenArray[0]);
+	}
+
+	char *fullPath = (char*)ht_get(h, tokenArray[0]);
+
+	//This is a hard coded LS, here we will call a function modified from kind.c
+	//to give us the file path if the command exists
+	if(fullPath != 0){
+	  //pid_t pid;
+	  tokenArray[tokenIndex] = 0;
+	  printf("Size of Parent list before resize is: %d\n", (int)sizeof(parentIds));
+	  if(pidSize <= pidIndex){
+	    pidSize = 2 * pidSize;
+	    printf("NEEDED SIZE: %d\n", sizeof(pid_t)*pidSize);
+	    printf("REsize parentIds!\n");
+	    //*parentIds = (pid_t*)resizeList(parentIds, pidIndex);	    
+	    
+
+	    parentIds = (pid_t*)realloc(parentIds, sizeof(pid_t)*pidSize);
+	    
+
+
+
+
+	    printf("Size of Parent list is: %d\n", (int)sizeof(parentIds));
+	  }
+
+	  printf("Size of needed list is: %d\n", (int)sizeof(pid_t)*pidIndex);
+	  printf("pidIndex is: %d\n", pidIndex);
+	  pidIndex++;
+	  parentIds[pidIndex-1] = fork();
+	  
+	  if(parentIds[pidIndex-1] == 0){
+	    //this ensures that the path was found
+	    if (fullPath) {
+	      int ret = execv(fullPath, tokenArray);
+	      int err = errno;
+	      if(ret == -1){
+		perror("Execvp");
+	      }
+	      
+	      //printf("Return value: %i   error number: %i\n",ret, err);
+	    }
+	  } else {
+	    pid_t commandId = wait(0);
+	    while(commandId != parentIds[pidIndex-1]){
+	      commandId = wait(0);
+	    }
+	  }	
+	}
+	
+	tokenIndex = 0; //reset the array to accept a new command
+      } else {
+	//Command has not ended yet, therefore
+	//Increment the token index.
+	tokenIndex++;
+	if (tokenIndex >= bSize) {
+	  //	  resizeList(tokenArray, tokenIndex);
+	  //bSize = 2*bSize;
+	}
+
+      }
+
+      //if flag is set, we have finished parsing      
+      if(flag == 1){ 
+	break;
+      }
+    } 
+    
+    printf("\n->"); 
+  }  
+  return 0;
+}
+
+
+
+/* Questions for Duane */
+// Clarify 0 stdin, 1 stdout, 2 stderr
+// :)
